d23dbbb0191ec0187fd1848cc0e4d2eeed1d7dc8
holdenghankins <hghankins@gmail.com>
2026-03-10 17:24:19 -0400

    Adding Base User Test Cases

--- src/test/java/bsu/edu/cs/cs222/characters/UserTest.java
+++ src/test/java/bsu/edu/cs/cs222/characters/UserTest.java
@@ -1,4 +1,59 @@
 package bsu.edu.cs.cs222.characters;
+import bsu.edu.cs.cs222.menues.SignUp;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 public class UserTest {
+    @Test
+    public void testAddPoints() {
+        User user = new User("Test", "test", 0);
+        user.addPoints(500);
+        assert user.getPoints() == 500;
+    }
+
+    @Test
+    public void testWager() {
+        User user = new User("Test", "test", 500);
+        boolean madeWager = user.makeWager(200);
+        assert madeWager;
+        assert user.getPoints() == 300;
+
+    }
+
+    @Test
+    public void testAllIn() {
+        User user = new User("Test", "test", 300);
+        boolean madeWager = user.makeWager(500);
+        assert !madeWager;
+        int allIn = user.allIn();
+        assert allIn == 300;
+    }
+
+    @Test
+    public void testReadUserFile() {
+        SignUp.signUp("Test", "StrongPass123!", "StrongPass123!");
+        User user = new User(Paths.get("src/main/resources/users/test.csv"));
+        assert user.getUsername().equals("Test");
+        assert user.getPoints() == 0;
+        File file = new File("src/main/resources/users/test.csv");
+        file.delete();
+
+    }
+    @Test
+    public void testSavePoints() {
+        SignUp.signUp("Test", "StrongPass123!", "StrongPass123!");
+        User user = new User(Paths.get("src/main/resources/users/test.csv"));
+        assert user.getUsername().equals("Test");
+        assert user.getPoints() == 0;
+        user.addPoints(500);
+        user.savePoints();
+
+        User userChecker = new User(Paths.get("src/main/resources/users/test.csv"));
+        assert (userChecker.getPoints() == 500);
+        File file = new File("src/main/resources/users/test.csv");
+        file.delete();
+    }
+
 }
