99f45e5746e40a62736e6cac6f9b43242eebe9a2
Treyford Mercer <thwmercer@gmail.com>
2026-03-15 15:09:41 -0400

    Clean up commented code for running.

--- src/main/java/bsu/edu/cs/cs222/games/liberty_bell/LibertyBellMachine.java
+++ src/main/java/bsu/edu/cs/cs222/games/liberty_bell/LibertyBellMachine.java
@@ -1,20 +1,20 @@
-//package bsu.edu.cs.cs222.games.liberty_bell;
-//import java.util.Objects;
-//import java.util.Random;
-//
-//public class LibertyBellMachine {
-//    private final LibertyBellSymbols[] strip;
-//    private final Random rng;
-//
-//    private int credits = 0;
-//    private int bet = 1;
-//
-//    public LibertyBellMachine() {
-//        this(new Random(), LibertyBellSymbols.values());
-//    }
-//
-//    public LibertyBellMachine(Random rng, LibertyBellSymbols[] strip) {
-//        this.rng = Objects.requireNonNull(rng, "rng");
-//        if (strip == null )
-//    }
-//}
+package bsu.edu.cs.cs222.games.liberty_bell;
+import java.util.Objects;
+import java.util.Random;
+
+public class LibertyBellMachine {
+    private final LibertyBellSymbols[] strip;
+    private final Random rng;
+
+    private int credits = 0;
+    private int bet = 1;
+
+    public LibertyBellMachine() {
+        this(new Random(), LibertyBellSymbols.values());
+    }
+
+    public LibertyBellMachine(Random rng, LibertyBellSymbols[] strip) {
+        this.rng = Objects.requireNonNull(rng, "rng");
+        if (strip == null )
+    }
+}
