5498829fd941dbec34cbafa305aec8c42c41115c
holdenghankins <hghankins@gmail.com>
2026-04-17 16:20:56 -0400

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,8 @@ tasks.test {
 }
 
 application {
-    mainClass.set("bsu.edu.cs.cs222.Launcher")
+    mainClass.set("bsu.edu.cs.cs222.games.roulette.RunRoulette")
+    //mainClass.set("bsu.edu.cs.cs222.Launcher")
     //mainClass.set("bsu.edu.cs.cs222.menues.leaderboard.RunLeaderboard");
 }
 
